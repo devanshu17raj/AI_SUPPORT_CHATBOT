1bf390c6bbb7b73997f9c75c83b48ef4cc0b85fb
Devanshu raj <devanshu17raj@gmail.com>
2026-06-01 19:14:17 +0530

    fix: updated NextRequest typings across all auth API routes

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,7 +1,7 @@
 import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req:NextResponse) {
+export async function GET(req:NextRequest) {
     const cookiestore=await cookies()
     cookiestore.delete("access_token")
     return NextResponse.redirect(`${process.env.NEXT_PUBLIC_APP_URL}`)
